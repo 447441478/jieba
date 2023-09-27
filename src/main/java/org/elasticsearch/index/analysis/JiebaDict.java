2462a55be3e51f37fec8e52cebbf20538d563538
宋进宇 <songjinyu@4399inc.com>
2023-09-27 16:26:32 +0800

    up

--- src/main/java/org/elasticsearch/index/analysis/JiebaDict.java
+++ src/main/java/org/elasticsearch/index/analysis/JiebaDict.java
@@ -29,12 +29,28 @@ public class JiebaDict {
         if (singleton == null) {
             synchronized (JiebaDict.class) {
                 if (singleton == null) {
-
+                    Properties properties = new Properties();
+                    long delay = 1000;
+                    long period = 60 * 1000;
+                    try {
+                        properties.load(Files.newInputStream(environment.pluginsFile().resolve("jieba/jieba.cfg.properties").toFile().toPath()));
+                        delay = Long.parseLong(properties.getOrDefault("remote.task.delay", delay).toString());
+                        period = Long.parseLong(properties.getOrDefault("remote.task.period", period).toString());
+                        if(delay < 0){
+                            delay = 0;
+                        }
+                        if(period < 0){
+                            period = 60 * 1000;
+                        }
+                    }catch (Exception e){
+                        logger.error(e);
+                    }
+                    logger.info("properties:{}", properties);
                     TimerTask task = new TimerTask() {
                         @Override
                         public void run() {
                             logger.info("start to load remote dict");
-                            boolean remoteChange = loadRemoteDic(environment);
+                            boolean remoteChange = loadRemoteDic(properties);
                             logger.info("start to load local dict");
                             WordDictionary.reload(environment.pluginsFile().resolve("jieba/dic").toFile(), remoteChange, remoteDict);
 //                            WordDictionary.getInstance()
@@ -43,7 +59,7 @@ public class JiebaDict {
                         }
                     };
 
-                    new Timer().scheduleAtFixedRate(task,1000, 60 * 1000);
+                    new Timer().scheduleAtFixedRate(task,delay, period);
 
                     singleton = new JiebaDict();
                     return singleton;
@@ -54,15 +70,13 @@ public class JiebaDict {
     }
 
 
-    private static boolean loadRemoteDic(Environment environment) {
+    private static boolean loadRemoteDic(Properties properties) {
         SpecialPermission.check();
-        return AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> loadRemoteDicUnprivileged(environment));
+        return AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> loadRemoteDicUnprivileged(properties));
     }
 
-    private static boolean loadRemoteDicUnprivileged(Environment environment) {
-        Properties properties = new Properties();
+    private static boolean loadRemoteDicUnprivileged(Properties properties) {
         try {
-            properties.load(Files.newInputStream(environment.pluginsFile().resolve("jieba/jieba.cfg.properties").toFile().toPath()));
             Object remoteDic = properties.getOrDefault("remote.ext.dic", "");
             String remoteUrl = remoteDic.toString();
             if(!remoteUrl.startsWith("http")){
@@ -70,11 +84,24 @@ public class JiebaDict {
                 System.out.println("remote dic url invalid remoteUrl:" + remoteUrl);
                 return false;
             }
+            int connectTimeout = 2000;
+            int readTimeout = 5000;
+            try {
+                connectTimeout = Integer.parseInt(properties.getOrDefault("remote.connectTimeout", connectTimeout).toString());
+                readTimeout = Integer.parseInt(properties.getOrDefault("remote.readTimeout", readTimeout).toString());
+                if(connectTimeout < 0){
+                    connectTimeout = 2000;
+                }
+                if(readTimeout < 0){
+                    connectTimeout = 5000;
+                }
+            }catch (NumberFormatException ignore){}
+
             URL url = new URL(remoteUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setConnectTimeout(2000);  // 设置连接超时时间为2秒
-            connection.setReadTimeout(5000);   // 设置读取超时时间为5秒
+            connection.setConnectTimeout(connectTimeout);  // 设置连接超时时间为2秒
+            connection.setReadTimeout(readTimeout);   // 设置读取超时时间为5秒
 
             String lastModifiedHeader = connection.getHeaderField("Last-Modified");
             if(Objects.isNull(lastModifiedHeader)){
